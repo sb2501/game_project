aff5dca342c74914dbfdfe0492b234bac65f058f
sboyd <sboyd@csumb.edu>
2017-12-28 23:58:04 +0000

    added graphics

--- app/src/index.js
+++ app/src/index.js
@@ -1,3 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
+
+class Rectangle extends React.Component{
+    
+    render(){
+       return(
+            <rect className="rect" width="50" height="100" x={this.props.x} y={this.props.y}></rect>
+        );
+    }
+}
+
+
+class Game extends React.Component{
+    
+    renderRect(_x,_y){
+        return <Rectangle x={_x} y={_y} />; 
+    }
+    
+    render(){
+        return(
+                
+            <svg width="100%">
+                {this.renderRect("90%","50")}
+                
+                {this.renderRect("10%","50")}
+        
+            </svg>
+            );
+    }
+}
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
